424ac7de72ae627b7eaf40959aed303961e1d61b
julinamaharjan <Julina.Maharjan@ibm.com>
2022-06-29 12:04:30 -0400

    inheriting IndividualOutcomeEstimator instead.

--- causallib/time_varying/GFormula.py
+++ causallib/time_varying/GFormula.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 
+import abc
 from typing import Optional, Any, OrderedDict
 import pandas as pd
-from causallib.time_varying.GFormulaBase import TimeVaryingBaseEstimator
 from ..estimation.base_estimator import IndividualOutcomeEstimator
+from sklearn.base import BaseEstimator as SKLearnBaseEstimator
+from .regression_curve_fitter import RegressionCurveFitter #TODO Referenced from Survival module. Assuming we need have similar wrapper class for fitting curve
 
 
 #TODO
@@ -12,12 +14,12 @@ class CovariateModels(OrderedDict):
     model: IndividualOutcomeEstimator
 
 
-class GFormulaBase(TimeVaryingBaseEstimator):
+class GFormulaBase(IndividualOutcomeEstimator):
     """
         GFormula base Estimator
     """
     def __init__(self,
-                 outcome_model: Any,
+                 outcome_model: None,
                  treatment_model: Any,
                  covariate_models: CovariateModels,
                  refit_models=True):
@@ -34,12 +36,21 @@ class GFormulaBase(TimeVaryingBaseEstimator):
 
         """
         super(GFormulaBase, self).__init__(lambda **x: None)
-
         self.outcome_model = outcome_model
-        self.treatment_model = treatment_model
-        self.covariate_models = covariate_models
+        self.treatment_model = self._init_regressor(treatment_model)
+        self.covariate_models = covariate_models #TODO call _init_regressor() for all covariate_models
         self.refit_models = refit_models
 
+    def _init_regressor(self, model):
+        """method to check and wrap the default learner class with RegressionCurveFitter."""
+        if isinstance(model, SKLearnBaseEstimator):
+            # Construct default curve fitter, parametric with a scikit-learn estimator
+            return RegressionCurveFitter(model)
+        else:
+            # Initialized lifelines RegressionFitter (or any implementation with a compatible API)
+            return model
+
+    @abc.abstractmethod
     def fit(self,
             X: pd.DataFrame,
             a: pd.Series,
@@ -64,6 +75,7 @@ class GFormulaBase(TimeVaryingBaseEstimator):
         """
         raise NotImplementedError
 
+    @abc.abstractmethod
     def estimate_individual_outcome(self,
                                     X: pd.DataFrame,
                                     a: pd.Series,
@@ -92,6 +104,7 @@ class GFormulaBase(TimeVaryingBaseEstimator):
               """
         pass
 
+    @abc.abstractmethod
     def estimate_population_outcome(self,
                                     X: pd.DataFrame,
                                     a: pd.Series,
@@ -120,6 +133,29 @@ class GFormulaBase(TimeVaryingBaseEstimator):
               """
         pass
 
+    @abc.abstractmethod
+    def apply_noise(self):
+        pass
+
+    @abc.abstractmethod
+    def prepare_data(self, pat_data):
+        pass
+
+    @staticmethod
+    def _predict_trajectory(self, X, a, t) -> pd.DataFrame:
+        """ Predicts the trajectories for all covariates X and treatment a.
+
+            Args:
+                  X (pd.DataFrame): Baseline covariate matrix of size (num_subjects, num_features).
+                  a (pd.Series): Treatment assignment of size (num_subjects,).
+                  t (pd.Series): Followup durations, size (num_subjects,).
+
+            Returns:
+                 pd.DataFrame: with time-step index, subject IDs (X.index and a) columns and
+                  point values for each column as entries
+        """
+        pass
+
 
 class GFormula(GFormulaBase):
     """
@@ -132,7 +168,7 @@ class GFormula(GFormulaBase):
                  refit_models=True
                  ):
 
-        super().__init__(outcome_model, treatment_model, covariate_models, refit_models)
+        super(GFormulaBase).__init__(outcome_model, treatment_model, covariate_models, refit_models)
 
     def fit(self,
             X: pd.DataFrame,
@@ -143,7 +179,13 @@ class GFormula(GFormulaBase):
             **kwargs
             ):
         if refit_models:
+
+            self.treatment_model.fit(df=X, duration_col=t, event_col=y,
+                                    **kwargs)
             self.treatment_model.fit(X, a)
+
+
+
             for cov in self.covariate_models:
                 self.covariate_models[cov].fit(X, a)
 
