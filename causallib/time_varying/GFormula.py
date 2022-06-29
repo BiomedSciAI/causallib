1c8d7f3dd1a19276049ba84be6e2984f70cdd942
julinamaharjan <Julina.Maharjan@ibm.com>
2022-06-28 22:27:36 -0400

    added few methods.

--- causallib/time_varying/GFormula.py
+++ causallib/time_varying/GFormula.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
 
-from typing import Optional, Any
+from typing import Optional, Any, OrderedDict
 import pandas as pd
-from causallib.time_varying.base_GFormula import TimeVaryingBaseEstimator
+from causallib.time_varying.GFormulaBase import TimeVaryingBaseEstimator
+from ..estimation.base_estimator import IndividualOutcomeEstimator
+
+
+#TODO
+class CovariateModels(OrderedDict):
+    covariate: str
+    model: IndividualOutcomeEstimator
 
 
 class GFormulaBase(TimeVaryingBaseEstimator):
@@ -10,9 +17,9 @@ class GFormulaBase(TimeVaryingBaseEstimator):
         GFormula base Estimator
     """
     def __init__(self,
-                 outcome_model,
-                 treatment_model,
-                 covariate_models: dict,
+                 outcome_model: Any,
+                 treatment_model: Any,
+                 covariate_models: CovariateModels,
                  refit_models=True):
         """
             outcome_model(IndividualOutcomeEstimator): A causal model that estimate on individuals level
@@ -27,16 +34,22 @@ class GFormulaBase(TimeVaryingBaseEstimator):
 
         """
         super(GFormulaBase, self).__init__(lambda **x: None)
-        delattr(self, "learner")
 
         self.outcome_model = outcome_model
         self.treatment_model = treatment_model
         self.covariate_models = covariate_models
         self.refit_models = refit_models
 
-    def fit(self, X, a, t, y, refit_models=True, **kwargs):
-        """ TODO --- Referenced from standardized_survival
-            Fits parametric models and calculates internal survival functions.
+    def fit(self,
+            X: pd.DataFrame,
+            a: pd.Series,
+            t: pd.Series,
+            y: Optional[Any] = None,
+            refit_models=True,
+            **kwargs
+            ):
+        """
+            Fits parametric models and calculates prediction curves.
 
             Args:
                 X (pd.DataFrame): Baseline covariate matrix of size (num_subjects, num_features).
@@ -51,10 +64,15 @@ class GFormulaBase(TimeVaryingBaseEstimator):
         """
         raise NotImplementedError
 
-    def estimate_population_outcome(self, X: pd.DataFrame, a: pd.Series, t: pd.Series, y: Optional[Any] = None,
+    def estimate_individual_outcome(self,
+                                    X: pd.DataFrame,
+                                    a: pd.Series,
+                                    t: pd.Series,
+                                    y: Optional[Any] = None,
                                     timeline_start: Optional[int] = None,
-                                    timeline_end: Optional[int] = None) -> pd.DataFrame:
-        """   TODO --- Referenced from standardized_survival
+                                    timeline_end: Optional[int] = None
+                                    ) -> pd.DataFrame:
+        """
               Returns individual estimated curves for each subject row in X/a/t
 
               Args:
@@ -74,17 +92,32 @@ class GFormulaBase(TimeVaryingBaseEstimator):
               """
         pass
 
-    def _prepare_data(self, X, a, t):
+    def estimate_population_outcome(self,
+                                    X: pd.DataFrame,
+                                    a: pd.Series,
+                                    t: pd.Series,
+                                    y: Optional[Any] = None,
+                                    timeline_start: Optional[int] = None,
+                                    timeline_end: Optional[int] = None
+                                    ) -> pd.DataFrame:
         """
-            placeholder method to clean and process the inputs: (X, a, t)
-            Args:
-                X (pd.DataFrame): Baseline covariate matrix of size (num_subjects, num_features).
-                a (pd.Series): Treatment assignment of size (num_subjects,).
-                t (pd.Series): Followup durations, size (num_subjects,).
+              Returns individual estimated curves for each subject row in X/a/t
 
-            Returns:
-                #TODO ?
-        """
+              Args:
+                  X (pd.DataFrame): Baseline covariate matrix of size (num_subjects, num_features).
+                  a (pd.Series): Treatment assignment of size (num_subjects,).
+                  t (pd.Series): Followup durations, size (num_subjects,).
+                  y: NOT USED (for API compatibility only).
+                  timeline_start (int): Common start time-step. If provided, will generate survival curves starting
+                                        from 'timeline_start' for all patients.
+                                        If None, will predict from first observed event (t.min()).
+                  timeline_end (int): Common end time-step. If provided, will generate survival curves up to 'timeline_end'
+                                      for all patients.
+                                      If None, will predict up to last observed event (t.max()).
+
+              Returns:
+                  pd.DataFrame: with time-step index, subject IDs (X.index) as columns and ??
+              """
         pass
 
 
@@ -101,11 +134,24 @@ class GFormula(GFormulaBase):
 
         super().__init__(outcome_model, treatment_model, covariate_models, refit_models)
 
-    def fit(self, X, a, t,  y, refit_models=True, **kwargs):
-        pass
+    def fit(self,
+            X: pd.DataFrame,
+            a: pd.Series,
+            t: pd.Series,
+            y: Optional[Any] = None,
+            refit_models=True,
+            **kwargs
+            ):
+        if refit_models:
+            self.treatment_model.fit(X, a)
+            for cov in self.covariate_models:
+                self.covariate_models[cov].fit(X, a)
+
+        self.outcome_model.fit(X, a, t, y)
+        return self
 
     def predict(self, X, a, t):
-        raise NotImplementedError("Predict is not well defined for doubly robust and thus unimplemented.")
+        pass
 
 
 
