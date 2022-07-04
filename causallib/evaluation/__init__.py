5af711276b24beb8463181525e23718ade3d5174
Michael M Danziger <Michael.Danziger@ibm.com>
2022-07-04 19:47:02 +0300

    unify plot functions to single function
    Signed-off-by: Michael M Danziger <Michael.Danziger@ibm.com>

--- causallib/evaluation/__init__.py
+++ causallib/evaluation/__init__.py
@@ -1,5 +1,5 @@
 """Objects and methods to evaluate accuracy of causal models."""
 from .evaluator import Evaluator
-from .plots.helpers import plot_evaluation_results, plot_single_evaluation_result
+from .plots.helpers import plot_evaluation_results
 
-__all__ = ["Evaluator", "plot_evaluation_results", "plot_single_evaluation_result"]
+__all__ = ["Evaluator", "plot_evaluation_results"]
