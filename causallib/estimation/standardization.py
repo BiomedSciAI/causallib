624baf87e831d567bbd5c2578c80aeb388015bc0
Ehud-Karavani <ehud.karavani@ibm.com>
2024-03-13 17:55:53 +0200

    Stringify `X.columns` if mixed column names types
    Signed-off-by: Ehud-Karavani <ehud.karavani@ibm.com>

--- causallib/estimation/standardization.py
+++ causallib/estimation/standardization.py
@@ -299,21 +299,34 @@ class Standardization(IndividualOutcomeEstimator):
             a_transformed = self.treatment_encoder_.transform(a.to_frame())
             a_transformed = a_transformed.toarray()
             a = pd.DataFrame(a_transformed, index=a.index, columns=self.treatment_encoder_.categories_[0])
-        a = self._align_treatment_name_types_for_join(X, a, a_name)
+        X, a = self._align_column_name_types_for_join(X, a, a_name)
         cur_X = g_tools.safe_join(a, X, join="outer")
         return cur_X
 
     @staticmethod
-    def _align_treatment_name_types_for_join(X, a, a_name):
-        """Align `a`'s columns/name type to match `X` so that joining
-        them creates homogeneous column names type and sklearn>=1.2 don't break."""
+    def _align_column_name_types_for_join(X, a, a_name):
+        """Align columns/name types in `X` and `a` to match so that joining them
+        creates homogeneous column names type and sklearn>=1.2 don't break."""
+        if a_name is None:
+            warnings.warn("`a.name` is None. Renaming to 'a'.")
+            a_name = "a"
+
         column_names_types = {type(c) for c in X.columns}
         if len(column_names_types) > 1:
+            X.columns = X.columns.astype(str)
             warnings.warn(
                 f"Column names of `X` contain mixed types ({column_names_types}),"
-                f"which sklearn>1.2 will raise for. Make sure column names are of the same type."
+                f"which sklearn>1.2 will raise for. "
+                f"Therefore `X.columns` were all converted to string."
             )
-            return a
+            if hasattr(a, "columns"):  # a DataFrame
+                a = a.add_prefix(f"{a_name}_")
+            elif hasattr(a, "name"):  # a Series
+                if type(a_name) != str:
+                    warnings.warn("`a.name` converted to str type to match `X.columns`.")
+                a.name = str(a_name)
+            return X, a
+
         column_names_type = column_names_types.pop()
 
         if hasattr(a, "columns"):  # a DataFrame
@@ -322,19 +335,19 @@ class Standardization(IndividualOutcomeEstimator):
             a_name_type = type(a.name)
 
         if a_name_type == column_names_type:
-            return a
+            return X, a
 
-        if column_names_type == str:  # a is int
+        if column_names_type == str:  # `a` is int
             if isinstance(a, pd.Series):
                 warnings.warn("Renamed `a.name` to 'a' to be of type string")
-                a.name = "a"
+                a.name = a_name
             if isinstance(a, pd.DataFrame):
                 a = a.add_prefix(f"{a_name}_")
-        if column_names_type == int:
+        if column_names_type == int:  # `a` is str
             # make sure
             if hasattr(a, "columns"):  # a DataFrame
                 a.columns = X.columns.min() - pd.Series(range(a.shape[1])) - 1
             elif hasattr(a, "name"):  # a Series
                 a.name = X.columns.min() - 1
                 warnings.warn(f"Renamed `a.name` to {a.name} to have unique column name from X.columns.")
-        return a
+        return X, a
