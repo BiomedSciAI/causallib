3fdaac469150c68e9ad3176e90e91de5b30b5020
Ehud-Karavani <ehud.karavani@ibm.com>
2024-03-13 15:04:53 +0200

    Standardization tests: replace unittest expectedFailure with assertRaise
    Currently, does not work.
    needs to figure out what happens with encoding when X cols are numeric
    (should not add prefix which makes it string, but also cannot be done
    numerically because 0 1 treatment columns will bash with 0 1 Xs columns)
    
    Signed-off-by: Ehud-Karavani <ehud.karavani@ibm.com>

--- causallib/tests/test_standardization.py
+++ causallib/tests/test_standardization.py
@@ -143,7 +143,6 @@ class TestStandardization(TestStandardizationCommon):
     def test_many_models(self):
         self.ensure_many_models()
 
-    @unittest.expectedFailure
     def test_mix_of_column_names_types(self):
         """Test for compatibility with scikit-learn's v1.2.0
         for a single type of column names
@@ -152,21 +151,29 @@ class TestStandardization(TestStandardizationCommon):
             X = self.data_lin["X"].rename(columns={0: "x"})
             a = self.data_lin["a"]
 
-            estimator = Standardization(LinearRegression(), encode_treatment=False)
-            estimator.fit(X, a, self.data_lin["y"])
+            with self.subTest("Non-encoded"):
+                with self.assertRaises(TypeError):
+                    estimator = Standardization(LinearRegression(), encode_treatment=False)
+                    estimator.fit(X, a, self.data_lin["y"])
 
-            estimator = Standardization(LinearRegression(), encode_treatment=True)
-            estimator.fit(X, a, self.data_lin["y"])
+            with self.subTest("Encoded"):
+                with self.assertRaises(TypeError):
+                    estimator = Standardization(LinearRegression(), encode_treatment=True)
+                    estimator.fit(X, a, self.data_lin["y"])
 
         with self.subTest("Test for numeric `X` and string `a`"):
             X = self.data_lin["X"]
             a = self.data_lin["a"].rename("a")
 
-            estimator = Standardization(LinearRegression(), encode_treatment=False)
-            estimator.fit(X, a, self.data_lin["y"])
+            with self.subTest("Non-encoded"):
+                with self.assertRaises(TypeError):
+                    estimator = Standardization(LinearRegression(), encode_treatment=False)
+                    estimator.fit(X, a, self.data_lin["y"])
 
-            estimator = Standardization(LinearRegression(), encode_treatment=True)
-            estimator.fit(X, a, self.data_lin["y"])
+            with self.subTest("Encoded"):
+                with self.assertRaises(TypeError):
+                    estimator = Standardization(LinearRegression(), encode_treatment=True)
+                    estimator.fit(X, a, self.data_lin["y"])
 
     def test_column_names_types(self):
         """Test for compatibility with scikit-learn's v1.2.0
