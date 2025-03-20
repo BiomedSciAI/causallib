5c406e7307f2513be291f3e6a103cabfb518a47f
Ehud-Karavani <ehud.karavani@ibm.com>
2025-03-20 16:53:40 +0200

    Merge positivity models tests into a single test file
    Signed-off-by: Ehud-Karavani <ehud.karavani@ibm.com>

--- causallib/positivity/tests/test_positivity_models.py
+++ causallib/positivity/tests/test_positivity_models.py
@@ -1,12 +1,94 @@
 import unittest
+
 import pandas as pd
-from causallib.positivity import UnivariateBoundingBox
-from causallib.positivity.univariate_bbox import QuantileContinuousSupport, Support, ContinuousSupport, CategoricalSupport
+from sklearn.linear_model import LogisticRegression
 from sklearn.exceptions import NotFittedError
 
-from causallib.positivity.datasets.positivity_data_simulator import (
-    make_1d_overlap_data, make_1d_normal_distribution_overlap_data
+from causallib.positivity import (
+    Trimming, UnivariateBoundingBox, Matching
 )
+from causallib.positivity.univariate_bbox import (
+    QuantileContinuousSupport, Support, ContinuousSupport, CategoricalSupport
+)
+from causallib.positivity.datasets.positivity_data_simulator import (
+    make_1d_overlap_data, make_1d_normal_distribution_overlap_data, make_random_y_like
+)
+
+
+class TestTrimming(unittest.TestCase):
+
+    # TODO: check the minimization criterion - following Crump's paper
+
+    @classmethod
+    def setUpClass(cls):
+        # Propensity model without regularization
+        cls.trimming = Trimming(
+            learner=LogisticRegression(solver="sag", penalty=None),
+            threshold="auto"
+        )
+
+    def test_is_fitted(self):
+        X, a = make_1d_normal_distribution_overlap_data()
+        self.trimming.fit(X, a)
+        self.assertTrue(hasattr(self.trimming.learner, "coef_"))
+        self.assertTrue(isinstance(self.trimming.threshold_, float))
+
+    def test_not_trimming_1d_same_distribution(self):
+        # Treated and control are drawn from the same distribution with
+        # probability to be treated = 0.5
+        X, a = make_1d_normal_distribution_overlap_data()
+        [X_trimmed, a_trimmed] = self.trimming.fit_transform(X, a)
+        self.assertEqual(a_trimmed.shape[0], a.shape[0])
+
+        # Treated and control are drawn from the same distribution with
+        # probability to be treated = 0.2
+        X, a = make_1d_normal_distribution_overlap_data(probability_treated=0.2)
+        [X_trimmed, a_trimmed] = self.trimming.fit_transform(X, a)
+        self.assertEqual(a_trimmed.shape[0], a.shape[0])
+
+    def test_trimming_different_distribution(self):
+        X, a = make_1d_normal_distribution_overlap_data(
+            treatment_params=(-2, 2),
+            control_params=(2, 1),
+            probability_treated=0.4,
+            n_samples=1000
+        )
+        [X_trimmed_crump, a_trimmed_crump] = self.trimming.fit_transform(X, a)
+        n_untrimmed_crump = a_trimmed_crump.shape[0]
+        with self.subTest("Trimmed samples with crump"):
+            self.assertTrue(n_untrimmed_crump < a.shape[0])
+
+        n_untrimmed_big_threshold = \
+            self.trimming.predict(X, a, threshold=0.25).sum()
+        with self.subTest("Trimmed samples with large threshold"):
+            self.assertTrue(n_untrimmed_big_threshold < a.shape[0])
+
+        with self.subTest("Test crump has more sample than large threshold"):
+            self.assertTrue(n_untrimmed_big_threshold < n_untrimmed_crump)
+
+    def test_pipeline(self):
+        X, a = make_1d_normal_distribution_overlap_data()
+
+        with self.subTest("Test fit with propensity model"):
+            self.trimming.fit(X, a)
+            self.assertTrue(True)  # Dummy assert for not thrown exception
+
+        with self.subTest("Test predict of the filtered data"):
+            self.trimming.predict(X, a)
+            self.assertTrue(True)  # Dummy assert for not thrown exception
+
+        with self.subTest("Test transform"):
+            self.trimming.transform(X, a)
+            self.assertTrue(True)  # Dummy assert for not thrown exception
+
+    def test_hardcoded_threshold_initialization_chooses_no_method(self):
+        self.trimming = Trimming(threshold=0.1)
+        self.assertFalse(hasattr(self.trimming, 'threshold'))
+
+    def test_default_initialization_chooses_crump(self):
+        self.trimming = Trimming()
+        self.assertEqual(self.trimming.threshold, "crump")
+
 
 
 def inbounds(bounds):
@@ -248,3 +330,42 @@ class TestUnivariateBBox(unittest.TestCase):
                 self.assertAlmostEqual(s1/s,ss1)
                 something_asserted = True
         self.assertTrue(something_asserted, "No asserts made, check data")
+
+
+class TestMatching(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.matching = Matching()
+
+    def test_1d_euclidean_caliper_1_match(self):
+        X, a= make_1d_overlap_data(treatment_bounds=(0, 75),
+                                      control_bounds=(25, 100))
+        self.matching.matching_transformer.set_params(metric="euclidean", caliper=1.0)
+        predict = self.matching.fit_predict(X, a)
+        Xp, ap= self.matching.transform(X, a,)
+        self.assertEqual(sum(predict), 100)
+        self.assertEqual(sum(predict), Xp.shape[0])
+        self.assertEqual(Xp.values.min(), 25)
+        self.assertEqual(Xp.values.max(), 74)
+
+    def test_transform_multiple_outputs(self):
+        X, a= make_1d_overlap_data(treatment_bounds=(0, 75),
+                                      control_bounds=(25, 100))
+        y = make_random_y_like(a)
+        t = make_random_y_like(a)
+
+        self.matching.matching_transformer.set_params(metric="euclidean", caliper=1.01)
+        self.matching.fit(X, a)
+
+        Xm, am = self.matching.transform(X, a)
+        unique_sample_counts = {i.shape[0] for i in (Xm, am)}
+        self.assertEqual(len(unique_sample_counts), 1)
+
+        Xm, am, ym= self.matching.transform(X, a, y)
+        unique_sample_counts = {i.shape[0] for i in (Xm, am, ym)}
+        self.assertEqual(len(unique_sample_counts), 1)
+
+        Xm, am, ym, tm = self.matching.transform(X, a, y, t)
+        unique_sample_counts = {i.shape[0] for i in (Xm, am, ym, tm)}
+        self.assertEqual(len(unique_sample_counts), 1)
